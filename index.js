60c89fbf7e949ef2acb07f017536834a128a3b10
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-26 19:22:04 -0500

    run update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+var gutil = require('gulp-util');
+
 module.exports = function (verb) {
   verb.task('default', function() {
-    verb.src(['.verb*.md', 'docs/.verb/**/*.md'])
+    verb.src(['.verb*.md', 'docs/_verb/**/*.md'])
+      .on('error', gutil.log)
       .pipe(verb.dest('./'));
   });
   return verb;
